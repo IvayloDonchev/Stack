1d5a10bb340a601e33fb409a9969d32cdfa39387
Ivaylo Donchev <idonchev@hotmail.com>
2017-03-16 16:42:40 +0200

    Add project files.

---
+++ Stack/MyStack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class MyStack<T>
+    {
+        private class Node
+        {
+            public T key;
+            public Node next;
+            public Node(T x)
+            {
+                key = x;
+                next = null;
+            }
+            public Node() : this(default(T)) { }
+        }
+        private Node sp;    // Stack Pointer
+        public void Push(T x)
+        {
+            Node p = new Node(x);
+            p.next = sp;
+            sp = p;
+        }
+        public T Pop()
+        {
+            if(sp != null)
+            {
+                T x = sp.key;
+                sp = sp.next;
+                return x;
+            }
+            throw new InvalidOperationException("Stack empty...");
+        }
+    }
+}
---
+++ Stack/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> stringStack =
+                new Stack<string>();
+            for (int i = 1; i <= 10; i++)
+                stringStack.Push(i.ToString());
+            try
+            {
+                while (true)
+                {
+                    string x = stringStack.Pop();
+                    Console.WriteLine(x.ToString() + " ");
+                }
+            }
+            catch(InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message + "\n");
+            }
+
+            MyStack<string> myStringStack =
+                new MyStack<string>();
+            for (int i = 1; i <= 10; i++)
+                myStringStack.Push(i.ToString());
+            try
+            {
+                while (true)
+                {
+                    string x = myStringStack.Pop();
+                    Console.Write(x.ToString() + " ");
+                }
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("\n"+e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
